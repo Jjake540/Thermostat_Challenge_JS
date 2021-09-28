f743088f7ee96d200971add2df7fbcf18e9b1a52
jake <jakebs540@gmail.com>
2021-09-28 23:08:38 +0100

    Thermostat: business logic complete

--- src/thermostat.js
+++ src/thermostat.js
@@ -5,26 +5,31 @@ class Thermostat {
     this.MINIMUM_TEMPERATURE = 10;
     this.MAX_LIMIT_PSM_ON = 25;
     this.MAX_LIMIT_PSM_OFF = 32;
-    this.temperature = 20;
+    this.DEFAULT_TEMPERATURE = 20;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+    this.HIGH_ENERGY_USAGE_LIMIT = 25;
+    this.temperature = this.DEFAULT_TEMPERATURE;
     this.powerSavingMode = true;
   }
 
   getCurrentTemperature() {
     return this.temperature;
   }
+  
   up() {
     if (this.isMinimumTempreature()) {
       return;
     }
-    this.temperature += 1
+    this.temperature += 1;
   }
+
   down() {
     if (this.isMinimumTempreature()) {
       return;
     }
-    this.temperature -= 1
+    this.temperature -= 1;
   }
-  
+
   isMinimumTempreature() {
     return this.temperature === this.MINIMUM_TEMPERATURE;
   }
@@ -42,9 +47,23 @@ class Thermostat {
   }
 
   isMaximumTempreature() {
-    if(this.isPowerSavingModeOn() === false) {
+    if (this.isPowerSavingModeOn() === false) {
       return this.temperature === this.MAX_LIMIT_PSM_OFF;
     }
     return this.temperature === this.MAX_LIMIT_PSM_ON;
+  }
+
+  resetTemperature() {
+    this.temperature = this.DEFAULT_TEMPERATURE;
+  }
+
+  energyUsage() {
+    if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+      return 'low-usage';
+    }
+    if (this.temperature <= this.HIGH_ENERGY_USAGE_LIMIT) {
+      return 'medium-usage';
+    }
+    return 'high-usage';
   }
 }
